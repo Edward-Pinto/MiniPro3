e4f5a92344e8d6074bda37a47f4aa36632b9261c
Edward-Pinto <edward.pinto@correounivalle.edu.co>
2024-05-28 18:37:17 -0500

    Se realizó la interfaz de usuario(vista)

--- Game/src/App.java
+++ Game/src/App.java
@@ -1,8 +1,6 @@
 import Modelo.Game;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        
         Game game = new Game();
         game.setEleccion_CPU();
         System.out.println(game.getEleccion_CPU());
